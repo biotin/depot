7f21c1271aabeee7960fb1be4049df407b736d96
biotin <biotin@debian.(none)>
2011-03-07 21:16:28 +0400

    Depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4bcb199c5da846947fe70c168c5682c85cc216df4c12fe992f92a0a0f1d3d2dc37eddca68cbf1df4c6ae6185fc80ce01616c25ef979848cd6f06e30e133d96c6'
