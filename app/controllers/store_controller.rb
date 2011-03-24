1ecc4cd15e75fa88a715125c846df81fc37d7ca3
biotin <biotin@debian.(none)>
2011-03-24 16:29:45 +0400

    Total Changes:Adding users,administrating,simple_css_changes

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,5 @@
 class StoreController < ApplicationController
+  skip_before_filter :authorize
   def index
     @products = Product.paginate(:page => params[:page], :per_page => 10)
     @cart= current_cart
